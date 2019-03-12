ce17ec5628f4625f88d65b884fa650f9bbd7af79
baxter2 <baxcallan@gmail.com>
2019-03-12 18:34:18 +1000

    udpate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,9 @@ class HomeController < ApplicationController
     @trash_em = List.where(:completed => true).destroy_all
     redirect_to lists_url, notice: "All Marked Items Were Successfully Deleted."  
   end
+  
+  def trash_em_all
+    @trash_em_all = List.all.destroy_all
+    redirect_to lists_url, notice: "All Items Were Successfully Deleted."  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :lists
 
   get 'home/trash_em'
+  get 'home/trash_em_all'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
