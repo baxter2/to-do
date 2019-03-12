129b8e7118ed13f53a457bdba10515d1843e3e3b
baxter2 <baxcallan@gmail.com>
2019-03-12 12:31:46 +1000

    $ rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+List.delete_all
+
+List.create!(description: 'Go to the gym',
+               completed: false)
+
+List.create!(description: 'Take out the trash',
+               completed: false)
+
+List.create!(description: 'Buy groceries',
+               completed: true)
+
+List.create!(description: 'Make coffee',
+               completed: false)
